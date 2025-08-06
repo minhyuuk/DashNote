bf689829072a6c17522b8894299fa4dd8d69d9de
minhyuuk <jlssteeve1@gmail.com>
2025-08-06 14:53:36 +0900

    feat: app icon 적용

--- app/src/main/java/com/minhyuuk/dashnote/ui/screen/MemoListScreen.kt
+++ app/src/main/java/com/minhyuuk/dashnote/ui/screen/MemoListScreen.kt
@@ -12,9 +12,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
